182bf2e6ed7478157c12ebe27383b9135cb8e2cb
duchoanghd <duchoanghd93@gmail.com>
2016-09-13 13:21:33 +0700

    Add new test

---
+++ cakoinhat/Controllers/TESTController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cakoinhat.Controllers
+{
+    public class TESTController : Controller
+    {
+        //
+        // GET: /TEST/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
